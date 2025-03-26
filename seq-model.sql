ef367f859fd87b9f634963a2b5b384ebe0ed9264
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2025-03-26 14:35:40 +0100

    update term names to correspond to dwc-dp

--- seq-model.sql
+++ seq-model.sql
@@ -4,15 +4,19 @@
 -- 3. order the result by sample id and relative abundance in descending order
 -- 4. return the sample id, taxon name, and relative abundance
 -- 5. the result should be like:
--- | sample_id | taxon_name | relative_abundance |
--- |-----------|------------|--------------------|
--- | 1         | taxon1     | 0.1                |
--- | 1         | taxon2     | 0.05               |
--- | 2         | taxon1     | 0.2                |
--- | 2         | taxon2     | 0.15               |  
+-- | eventID   | scientificName | relativeAbundanceInSample |
+-- |-----------|----------------|--------------------|
+-- | 1         | taxon1         | 0.1                |
+-- | 1         | taxon2         | 0.05               |
+-- | 2         | taxon1         | 0.2                |
+-- | 2         | taxon2         | 0.15               |  
 
-SELECT e.id, i.taxon_name, anl_seq.readCount/a.sampleSizeValue as relative_abundance
-FROM SamplingEvent e JOIN Analysis a ON e.id = a.event_id JOIN AnalysisSequence anl_seq ON a.id = anl_seq.analysis_id JOIN Sequence s ON anl_seq.sequence_id = s.id JOIN Identification i on s.id = i.sequence_id
+SELECT e.eventID, t.scientificName, anl_seq.readCount/a.sampleSizeValue as relativeAbundanceInSample
+FROM Event e JOIN geneticAnalysis a ON e.eventID = a.eventID 
+JOIN geneticAnalysisSequence anl_seq ON a.geneticAnalysisID = anl_seq.geneticAnalysisID 
+JOIN geneticSequence s ON anl_seq.geneticSequenceID = s.geneticSequenceID 
+JOIN Identification i on s.geneticSequenceID = i.identificationBasedOnGeneticSequenceID
+JOIN Taxon t on i.taxonID = t.taxonID
 WHERE a.pcr_primer_fwd = "CCTACGGGNGGCWGCAG" AND a.pcr_primer_rev = "GACTACHVGGGTATCTAATCC" 
 HAVING relative_abundance > 0.01
 ORDER BY e.id, relative_abundance DESC
